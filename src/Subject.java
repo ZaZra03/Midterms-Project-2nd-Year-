f64a23958e8429c7e3ae457263986e3b6699b7a3
Mark Christian Banagan <112263674+ChrisOfReality@users.noreply.github.com>
2022-10-21 22:22:51 +0800

    Update Subject.java
    Added DisplayDetails() logic

--- src/Subject.java
+++ src/Subject.java
@@ -2,31 +2,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subject {
-	//class fields
+	// class fields
 	private int id;
 	private String subjectName;
 	private int units;
-	private int idCounter = 1;
+	private static int idCounter = 1;
 	private List<Student> listStudent = new ArrayList<Student>();
-	
-	//constructor
+
+	public int getId() {
+		return id;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public List<Student> getListStudent() {
+		return listStudent;
+	}
+
+	// constructor
 	public Subject(String subjectName, int units) {
 		this.id = idCounter;
 		this.subjectName = subjectName;
 		this.units = units;
 		idCounter++;
 	}
-	
-	//methods
+
+	// methods
 	public void EnlistStudent(Student objStudent) {
-		
+
 	}
-	
+
 	public void RemoveStudent(int studentId) {
-		
+
 	}
-	
+
 	public void DisplayDetails() {
-		
+		System.out.println("\nSUBJECT ID : " + this.id);
+		System.out.println("SUBJECT NAME : " + this.subjectName);
+		System.out.println("NUMBER OF UNITS : " + this.units);
 	}
 }
