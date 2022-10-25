56742f37c2ba91dc371dada3b5f22e36492c403a
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2022-10-25 21:36:07 +0800

    Update Subject.java

--- src/Subject.java
+++ src/Subject.java
@@ -1,30 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Subject {
-	// class fields
+public class Subject { //Name9 Start
+	// Class Fields
 	private int id;
 	private String subjectName;
 	private int units;
-	private static int idCounter = 1;
-	private List<Student> listStudent = new ArrayList<Student>();
+	private static int idCounter = 6500;
+	private List<Student> listStudents = new ArrayList<Student>();
 
+	// Getters
 	public int getId() {
-		return this.id;
+		return id;
 	}
-	public int getUnits() {
-		return this.units;
-	}
-	
+
 	public String getSubjectName() {
-		return this.subjectName;
+		return subjectName;
 	}
 
-	public List<Student> getListStudent() {
-		return this.listStudent;
-	}
-
-	// constructor
+	// Parameterized Constructor
 	public Subject(String subjectName, int units) {
 		this.id = idCounter;
 		this.subjectName = subjectName;
@@ -32,25 +26,60 @@ public class Subject {
 		idCounter++;
 	}
 
-	// methods
-	public void EnlistStudent(Student objStudent) {
-		boolean isStudentEnlisted = false;
-		for(int i = 0; i < listStudent.size(); i++) {
-			if(listStudent.get(i) == objStudent) {
-				isStudentEnlisted = true;
-				break;
+	// Methods
+	public void AddStudent(Student objStudent) {
+		this.listStudents.add(objStudent);
+	}
+
+	public void EnlistStudent(Student objStudent) throws Exception {
+		for (Student student : this.listStudents) {
+			if (student.getId() == objStudent.getId()) {
+				throw new Exception("\nThis Student is already enlisted to this Subject");
 			}
 		}
-		if(isStudentEnlisted == false) this.listStudent.add(objStudent);
+		this.AddStudent(objStudent);
+		objStudent.AddSubject(this);
+	} //Name9 End
+
+	public void RemoveStudent(int studentId) throws Exception { //Name10 Start
+		for (int i = 0; i < this.listStudents.size(); i++) {
+			if (this.listStudents.get(i).getId() == studentId) {
+				this.listStudents.remove(i);
+				return;
+			}
+		}
+		throw new Exception("\nStudent ID not found on this Subject's Enlisted Students");
 	}
 
-	public void RemoveStudent(int studentId) {
-		listStudent.remove(studentId);
-	}
-
-	public void DisplayDetails() {
+	public void DisplaySubjectDetails() {
 		System.out.println("\nSUBJECT ID : " + this.id);
 		System.out.println("SUBJECT NAME : " + this.subjectName);
 		System.out.println("NUMBER OF UNITS : " + this.units);
 	}
+
+	public void DisplayEnlistedStudents() {
+		if (this.listStudents.size() == 0) {
+			System.out.println("\nNO STUDENTS ENLISTED\n");
+		} else {
+			for (Student student : this.listStudents) {
+				System.out.println(student.getId() + " - " + student.GetFullName());
+			}
+		}
+	}
+
+	public void DisplayFullDetails() {
+		System.out.println("\n==================================");
+		System.out.println("\nSUBJECT ID : " + this.id);
+		System.out.println("SUBJECT NAME : " + this.subjectName);
+		System.out.println("NUMBER OF UNITS : " + this.units);
+		System.out.println("\nSTUDENTS ENLISTED");
+		if (this.listStudents.size() == 0) {
+			System.out.println("\nNO STUDENTS ENLISTED\n");
+		} else {
+			for (Student student : this.listStudents) {
+				student.DisplayStudentDetails();
+			}
+		}
+		System.out.println("==================================");
+	} //Name10 End
 }
